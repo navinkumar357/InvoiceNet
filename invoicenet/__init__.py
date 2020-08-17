d28cbf89edfd1a9f5bd05ce2cf505ac1158b51c8
Navinkumar Palanisamy <36514924+navinkumar357@users.noreply.github.com>
2020-08-17 21:32:34 +0200

    Update __init__.py

--- invoicenet/__init__.py
+++ invoicenet/__init__.py
@@ -28,10 +28,10 @@ FIELD_TYPES = {
 FIELDS = dict()
 
 FIELDS["invoice_number"] = FIELD_TYPES["general"]
-# FIELDS["vendor_name"] = FIELD_TYPES["general"]
+FIELDS["vendor_name"] = FIELD_TYPES["general"]
 
-# FIELDS["invoice_date"] = FIELD_TYPES["date"]
+FIELDS["invoice_date"] = FIELD_TYPES["date"]
 
 FIELDS["net_amount"] = FIELD_TYPES["amount"]
-# FIELDS["tax_amount"] = FIELD_TYPES["amount"]
-# FIELDS["total_amount"] = FIELD_TYPES["amount"]
+FIELDS["tax_amount"] = FIELD_TYPES["amount"]
+FIELDS["total_amount"] = FIELD_TYPES["amount"]
